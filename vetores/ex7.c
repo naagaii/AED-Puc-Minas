7761ee529e9a8938182374f1203cf7451d92ea7e
Francielle <152073275+naagaii@users.noreply.github.com>
2024-05-22 12:40:05 -0300

    restante exercícios

--- vetores/ex7.c
+++ vetores/ex7.c
@@ -1,10 +1,27 @@
 #include <stdio.h>
-#define tam 10
 
 //Implemente um algoritmo de ordenação para ordenar um vetor de inteiros em ordem
 //decrescente
 
-int main(){
-    int vetor[tam]={2, 5, 8, 7, 4, 1, 2, 9, 3, 6};
-    
+void decrescente(int *vetor, int tamanho) ;
+#define tam 10
+int main() {
+int vetor[tam] = {5, 3, 8, 4, 2, 9, 1, 7, 6, 10};
+decrescente(vetor, tam);
+for (int i = 0; i < 10; i++) {
+printf("%d ", vetor[i]);
+}
+return 0;
+}
+void decrescente(int *vetor, int tamanho) {
+int temp;
+for (int i = 0; i < tamanho - 1; i++) {
+for (int j = 0; j < tamanho - i - 1; j++) {
+if (vetor[j] < vetor[j + 1]) {
+temp = vetor[j];
+vetor[j] = vetor[j + 1];
+vetor[j + 1] = temp;
+}
+}
+}
 }
